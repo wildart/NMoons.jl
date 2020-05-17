8421392438e1a779918cadf8da4461e8461f531d
Art Wild <wildart@gmail.com>
2020-05-17 14:26:06 -0400

    defiend translation & repulsion for TM

--- src/nmoons.jl
+++ src/nmoons.jl
@@ -1,5 +1,3 @@
-using Random
-
 """
     nmoons(T, n, c; kwargs...) -> (Matrix{T}, Vector{Int})
 
@@ -14,20 +12,22 @@ Keyword parameters:
 - `d`: dimension of the full space
 - `r`: radius of the half-circle
 - `ε`: variance of a random Gaussian noise
-- `translation::Vector{T}`: half-circle subsets translation parameters (must be of size `d`)
+- `repulse::Tuple{T,T}`: half-circle subsets translation parameters
+- `translation::Vector{T}`: dataset translation vector
 - `rotations::Dict{Pair{Int,Int},T}`: dataset rotation parameters. Each rotation is specified as angle between particular pair of axes
 - `shuffle`: perform point shuffling in the dataset, default value `true`
 - `seed`: RNG seed value (if it's `nothing` then RNG will not be initialized), default value `nothing`
 
 Here is an example of dataset generation:
 ```jldoctest
-julia> X, L = nmoons(Float64, 100, 2,        # Generate 200 Float64 points divided on two susbsets
-                     ε=0.0, d=3,             # in 3D space with no noise
-                     translation=[0.25;0;0], # translated from origin
+julia> X, L = nmoons(Float64, 100, 2,      # Generate 200 Float64 points divided on two susbsets
+                     ε=0.0, d=3,           # in 3D space with no noise
+                     repulse=(0.25,0.0),   # repulse half-spheres in x-direction by r/4
+                     translation=[0.25;0], # translated from origin
                      rotations=Dict((1=>3)=>π/3, (1=>2)=>-π/3)); # rotated 30° from X to Z, -30° from X to Y
 
 julia> X
-3×100 Array{Float64,2}:
+3×200 Array{Float64,2}:
  0.125      0.179973   0.233692  …   0.262203   0.319      0.375
 -0.216506  -0.183582  -0.149013     -0.709903  -0.680664  -0.649519
  0.433013   0.431233   0.425903      1.29193    1.29726    1.29904
@@ -35,30 +35,32 @@ julia> X
 """
 function nmoons(::Type{T}, m::Int=100, c::Int=2;
                 shuffle::Bool=false, ε::Real=0.1, d::Int = 2, r::Real=1.0,
-                translation::Vector{T}=zeros(T, d),
+                repulse::Tuple{T,T}=(zero(T),zero(T)),
+                translation::Vector{T}=fill(zero(T),d),
                 rotations::Dict{Pair{Int,Int},T} = Dict{Pair{Int,Int},T}(),
                 seed::Union{Int,Nothing}=nothing) where {T <: Real}
+    @assert d > 1 "The ambient dimention must be grater than 1"
+    @assert length(translation) == d "The dimension of the translation vector must be $d"
+
     rng = seed === nothing ? Random.GLOBAL_RNG : MersenneTwister(Int(seed))
     n = c*m
-    ssize = floor(Int, m)
-    ssizes = fill(ssize, c)
-    ssizes[end] += n - ssize*c
+    ssizes = fill(m, c)
+    ssizes[end] += n - m*c
     @assert sum(ssizes) == n "Incorrect partitioning"
     pi = convert(T, π)
-    R(θ) = [cos(θ) -sin(θ); sin(θ) cos(θ)]
     X = zeros(d,0)
     for (i, s) in enumerate(ssizes)
         pts = range(zero(T), pi, length=s)
         circ_x = cos.(pts).-1.0
         circ_y = sin.(pts)
-        C = R(-(i-1)*(2*pi/c)) * hcat(circ_x, circ_y)'
-        C = vcat(C, zeros(d-2, s))
-        dir = C[:,end]-zeros(d) # translation direction
+        C = rotate2d(-(i-1)*(2*pi/c)) * hcat(circ_x, circ_y)'
+        dir = C[:,end] # repulsion direction from origin
         dir ./= abs(sum(dir))   # normalize directions
-        @debug "Dimension $i"  dir="$dir" farthest="$(C[:,end])"
-        translate = dir.*r.*translation
-        @debug "Translation $i"  translate="$translate"
-        X = hcat(X, C .+ translate)
+        @debug "Half-circle $i"  dir="$dir"
+        translate = dir.*r.*repulse
+        @debug "Repulse $i"  translate="$translate"
+        C = vcat(C .+ translate, zeros(d-2, s)) # translate & pad coordinates with 0s
+        X = hcat(X, C)
     end
     # generate labels
     y = vcat([fill(i,s) for (i,s) in enumerate(ssizes)]...)
@@ -73,7 +75,9 @@ function nmoons(::Type{T}, m::Int=100, c::Int=2;
     end
     # rotate dataset
     for ((i,j),θ) in rotations
-        X[[i,j],:] .= R(θ)*view(X,[i,j],:)
+        X[[i,j],:] .= rotate2d(θ)*view(X,[i,j],:)
     end
+    # translate dataset
+    X .+= translation
     return X, y
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,15 +6,36 @@ using NMoons, Test
     d = 2
     ε = 0.0
     r = 2.0
+    rp = (1.0, 0.0)
     t = [5.0, 0.0]
     θ = Dict((1=>2) => π/2)
 
-    X, L = nmoons(Float64, m, c, ε=ε, d=d, r=r, translation=t, rotations=θ)
+    X, L = nmoons(Float64, m, c, ε=ε, d=d, r=r, repulse=rp, translation=t, rotations=θ)
     @test size(X) == (d, c*m)
     @test length(L) == c*m
     @test unique(L) == collect(1:c)
-    @test X[2,1] ≈ -t[1]*r
+    @test X[1,1] ≈ t[1]
+    @test X[2,1] ≈ -rp[1]*r
+    @test X[1,m] ≈ t[1]
     @test X[2,m] ≈ -t[1]*r-r
+    @test X[1,m+1] ≈ t[1]
     @test X[2,m+1] ≈ t[1]*r
+    @test X[1,c*m] ≈ t[1]
     @test X[2,c*m] ≈ t[1]*r+r
+
+    c = 3
+    d = 3
+    rp = (0.25, 0.25)
+    t = [0.0, 1.0, 0.0]
+    X, L = nmoons(Float64, m, c, ε=ε, d=d, r=r, repulse=rp, translation=t)
+    @test size(X) == (d, c*m)
+    @test length(L) == c*m
+    @test unique(L) == collect(1:c)
+    @test X[3,1] ≈ 0.0
+    @test X[1,1] ≈ -r*rp[1]
+    @test X[2,1] ≈ t[2]
+end
+
+@testset "Spheres" begin
+
 end
